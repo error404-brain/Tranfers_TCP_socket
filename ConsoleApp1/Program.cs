28242ed5f53541d64e0165d2c141b3b3e0d97f4f
Hưng Dõ <vokhanhhung03@gmail.com>
2024-12-31 11:25:26 +0700

    1.1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+class FileSender
+{
+    static void Main(string[] args)
+    {
+        const string serverIp = "127.0.0.1"; 
+        const int port = 9000;
+        string filePath = @"G:\path_to_your_file.txt"; 
+
+        try
+        {
+            using (TcpClient client = new TcpClient(serverIp, port))
+            using (NetworkStream stream = client.GetStream())
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    stream.Write(buffer, 0, bytesRead);
+                }
+            }
+
+            Console.WriteLine("File sent successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+class FileServer
+{
+    static void Main(string[] args)
+    {
+        const int port = 9000;
+        string savePath = @"G:\hianhvinh.txt";
+
+
+        TcpListener listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+        Console.WriteLine("Server active...");
+
+        using (TcpClient client = listener.AcceptTcpClient())
+        {
+            Console.WriteLine("Client connect!");
+            using (NetworkStream stream = client.GetStream())
+            using (FileStream fs = new FileStream(savePath, FileMode.Create, FileAccess.Write))
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fs.Write(buffer, 0, bytesRead);
+                }
+            }
+        }
+
+        Console.WriteLine($"File da duoc gui thanh cong: {savePath}");
+        listener.Stop();
+    }
+}
